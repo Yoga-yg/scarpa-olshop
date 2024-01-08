0736ee0f0a960b771b53776436f530e259318602
Yoga-yg <151545309+Yoga-yg@users.noreply.github.com>
2024-01-08 18:01:21 +0700

    Add files via upload

---
+++ tentang-kami.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online | Tentang Kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php require "navbar.php"; ?>
+
+    <div class="container-fluid banner2 d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">Tentang Kami</h1>
+        </div>
+    </div>
+    
+    <div class="container-fluid py-5">
+        <div class="container fs-5 text-center">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, non natus deleniti atque ratione quidem incidunt! Quia, aspernatur magni quibusdam alias voluptate repudiandae, magnam totam nemo natus laudantium sed aperiam veniam voluptatum similique iste aut laboriosam deleniti ea assumenda distinctio dolor enim blanditiis id in. Quam odit molestias tenetur maiores provident velit, tempora, numquam corporis ex ducimus aliquid neque quod facere similique modi corrupti itaque nobis quia. Dolorem laudantium eaque, non repudiandae quo quibusdam accusantium quod officia placeat repellendus incidunt blanditiis, distinctio, culpa architecto. Mollitia esse exercitationem harum porro reiciendis maxime ipsam. Nam culpa temporibus fugit dolores qui sunt laborum?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, cupiditate quibusdam. Debitis officiis omnis earum reprehenderit numquam accusantium quasi totam ullam, expedita sequi facilis dolor nobis ratione, architecto impedit modi fugit! Dicta quo eveniet veritatis maxime unde. Omnis, aperiam animi saepe culpa atque aliquid provident natus architecto molestias, accusantium soluta.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel vitae dolore architecto minima, rerum numquam itaque et provident fugit maxime corrupti error. Ratione esse laboriosam exercitationem quaerat aut assumenda vitae incidunt, nesciunt architecto, corrupti reiciendis rem alias sequi, commodi quam eius nam eveniet minima dolores ipsam impedit quas nihil. Adipisci quibusdam nobis, nostrum totam quas inventore ratione, unde ad nihil ut dolorem animi nisi nam, optio eveniet explicabo soluta neque quisquam doloribus. Sapiente qui pariatur quis quaerat aspernatur facere rerum!
+            </p>
+        </div>
+    </div>
+    
+    <?php require "footer.php"; ?>
+    
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="fontawesome/js/all.min.js"></script>
+</body>
+</html>
